14aa76fd4551d2f7a602a896fd24be189a9f3582
tendai <pfidzetendai@gmail.com>
2024-12-09 17:22:25 +0000

    github workflow

--- deploy/install_controller.ps1
+++
@@ -1,7 +0,0 @@
-helm repo add eks https://aws.github.io/eks-charts
-
-helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller --namespace kube-system --set clusterName=pfitz-kube-1 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller
-
-kubectl -n kube-system rollout status deployment aws-load-balancer-controller
-
-kubectl get deployment -n kube-system aws-load-balancer-controller
