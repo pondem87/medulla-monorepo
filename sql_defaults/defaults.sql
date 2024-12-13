1e3ef7a0b38df6855c486def92a3285163069974
Tendai Pfidze <pfidzetendai@gmail.com>
2024-12-13 13:41:26 +0200

    added sql for default objects

---
+++ sql_defaults/defaults.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.llm_model ("name", "type", "costPerInputToken", "costPerOutputToken", "costMultiplier") 
+VALUES ('gpt-4o-mini', 'chat', 30, 120, 100000000);
+
+INSERT INTO public.currency ("name", "isoCode", "toBaseCurrencyMultiplier")
+VALUES ('Botswana Pula', 'BWP', 13.71);
