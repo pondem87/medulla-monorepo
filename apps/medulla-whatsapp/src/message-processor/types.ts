1d95b3b3862eedaf89350ce4a7fbe764a4e8e579
Tendai Pfidze <pfidzetendai@gmail.com>
2025-01-12 18:02:59 +0200

    fixed messages import

--- apps/medulla-whatsapp/src/message-processor/types.ts
+++ apps/medulla-whatsapp/src/message-processor/types.ts
@@ -1,6 +1,6 @@
 import { StateMachineActor } from "@app/medulla-common/common/extended-types"
 import { ISMContext, ISMEventType } from "./interactive.state-machine.provider"
-import { Messages } from "./dto/message.dto"
+import { Messages } from "@app/medulla-common/common/whatsapp-api-types"
 
 export interface IRunStates {
     runImplementedStates(ismActor: StateMachineActor<ISMEventType, ISMContext>, message: Messages): Promise<boolean>
